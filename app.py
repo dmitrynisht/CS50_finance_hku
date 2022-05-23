84484088eeb2040ff3f64ead0d9dbf115bbcb881
dmitrynisht <dmitrynisht@mail.ru>
2022-05-24 00:04:23 +0300

    testing hku index

--- app.py
+++ app.py
@@ -847,6 +847,19 @@ def get_portfolio_with_prices(**kwargs):
         GROUP BY hist1.symbol
         HAVING SUM(shares) > 0) AS balance
     """)
+    stmt_last_prices = ("""
+        SELECT
+            symbol,
+            name,
+            SUM(shares) AS shares
+        FROM history AS hist1
+        INNER JOIN
+            (SELECT
+                (:user_id) AS user_id,
+                (:dont_filter_by_symbol) AS dont_filter_by_symbol,
+                (:f_symbol) AS f_symbol) AS filter
+        ON hist1.user_id = filter.user_id
+    """)
     dont_filter_by_symbol = kwargs['dont_filter_by_symbol'] if ('dont_filter_by_symbol' in kwargs) else True
     symbol = '' if dont_filter_by_symbol else kwargs['symbol']
     rows = db.execute(
